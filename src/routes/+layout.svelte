3f11ddb25f75fe6811ff966b4d67bca2e4db219c
Gabriel <B51095@fgv.edu.br>
2025-03-27 12:04:21 -0300

    Adiciona layout base e atualiza configurações globais

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+
+
+<script>
+import { base } from "$app/paths";
+import { page } from "$app/stores";
+let pages = [
+    { url: "/", title: "Home" },
+    { url: "/projects", title: "Projects" },
+    { url: "/contact", title: "Contact" },
+    { url: "/resume", title: "Resume" },
+    {url: "https://github.com/Mike-Kowalski", title:"Github"}
+];
+</script>
+
+<nav>
+  {#each pages as p}
+  <a
+    href={p.url.startsWith("http") ? p.url: `${base}${p.url}`}
+    class:current={$page.route.id === p.url}
+    target={p.url.startsWith("http") ? "_blank" : undefined}
+>
+    {p.title}
+</a>
+{/each}
+
+
+
+</nav>
+
+
+YOLO <slot />
